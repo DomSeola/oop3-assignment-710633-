50ef72f780a23ba35f3a5b87828b64f9b9122af1
DomSeola <DomiLenar@wp.pl>
2025-07-07 18:05:11 +0200

    updated omdb client

--- src/main/java/MovieWatchlist/OOP3/service/OmdbClient.java
+++ src/main/java/MovieWatchlist/OOP3/service/OmdbClient.java
@@ -1,5 +1,35 @@
 package MovieWatchlist.OOP3.service;
 
-public class OmdbClient {
+import MovieWatchlist.OOP3.dto.OmdbMovie;
+import MovieWatchlist.OOP3.exception.OmdbApiException;
+import org.apache.http.client.utils.URIBuilder;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Service;
+import java.net.URI;
+import java.net.URISyntaxException;
 
+@Service
+public class OmdbClient {
+ @Value("${omdb.api.key}")
+ private String apiKey;
+ 
+ private final ApiService apiService;
+ 
+ public OmdbClient(ApiService apiService) {
+     this.apiService = apiService;
+ }
+ 
+ public OmdbMovie fetchMovie(String title) {
+     try {
+         URI uri = new URIBuilder("http://www.omdbapi.com/")
+             .addParameter("apikey", apiKey)
+             .addParameter("t", title)
+             .build();
+         return apiService.executeGetRequest(uri, OmdbMovie.class);
+     } catch (URISyntaxException e) {
+         throw new OmdbApiException("Invalid URI for OMDb API", e);
+     } catch (Exception e) {
+         throw new OmdbApiException("Failed to fetch from OMDb API", e);
+     }
+ }
 }
